f93694a5ceb69081d7e924973ade9d41ab6fe838
Ana Silva <annadasilva89@hotmail.com>
2023-07-05 01:41:43 -0300

    Seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_cadarpio(name, cpf, email) VALUES('nana', '6059839089', 'nana@gmail.com');
+INSERT INTO tb_cadarpio(name, cpf, email) VALUES('maria', '95107711049', 'maria@gmail.com');
+INSERT INTO tb_cadarpio(name, cpf, email) VALUES('joana', '02824106000', 'joana@gmail.com');
+INSERT INTO tb_cadarpio(name, cpf, email) VALUES('ana', '10718887069', 'ana@gmail.com');
+INSERT INTO tb_cadarpio(name, cpf, email) VALUES('rita', '25800019053', 'rita@gmail.com');
