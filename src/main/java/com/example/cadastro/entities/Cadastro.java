1347b1a86e4af2644bfce4ab15df5573f7e161a7
Ana Silva <annadasilva89@hotmail.com>
2023-07-05 01:19:10 -0300

    Properties files, cadastro entity, ORM

---
+++ src/main/java/com/example/cadastro/entities/Cadastro.java
@@ -0,0 +1,73 @@
+package com.example.cadastro.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_cadarpio")
+public class Cadastro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String email;
+
+    public Cadastro() {
+
+    }
+
+    public Cadastro(Long id, String name, String cpf, String email) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cadastro cadastro = (Cadastro) o;
+        return Objects.equals(id, cadastro.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
